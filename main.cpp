a694537293047ef03cb1ca497315904389535d5f
Avinash R Kumar <avinashk19@kpit.com>
2025-07-28 10:12:41 +0530

    Addnumber function - dat 1

--- main.cpp
+++ main.cpp
@@ -1,6 +1,14 @@
 #include <iostream>
 using namespace std;
 
+//function to add two numbers
+
+int addNumber(int a, int b) {
+
+	return a+b;
+
+}
+
 int main() {
     cout << "Hello World" << endl;
     return 0;
