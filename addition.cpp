e0f17e4a60a7c1f4710d73ab76a71ca3125a8fbc
Avinash R Kumar <avinashk19@kpit.com>
2025-10-01 11:07:24 +0530

    addition program
    Signed-off-by: Avinash R Kumar <avinashk19@kpit.com>

---
+++ addition.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+ 
+int main() {
+    cout << "===== Addition of Two Numbers =====" << endl;
+    int num1, num2, sum;
+    // Get input from user
+    cout << "Enter first number: ";
+    cin >> num1;
+    cout << "Enter second number: ";
+    cin >> num2;
+    // Calculate sum
+    sum = num1 + num2;
+    // Display result
+    cout << "\nResult: " << num1 << " + " << num2 << " = " << sum << endl;
+    return 0;
+}
